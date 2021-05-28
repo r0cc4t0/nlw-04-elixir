e3675061591bccef612b9fb12db64df41cf644c6
r0cc4t0 <guilherme.roccato@outlook.com>
2021-05-27 23:58:23 -0300

    Transfer between Accounts

--- lib/rocketpay/accounts/withdrawal.ex
+++ lib/rocketpay/accounts/withdrawal.ex
@@ -11,7 +11,7 @@ defmodule Rocketpay.Accounts.Withdrawal do
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{update_balance: account}} -> {:ok, account}
+      {:ok, %{withdrawal: account}} -> {:ok, account}
     end
   end
 end

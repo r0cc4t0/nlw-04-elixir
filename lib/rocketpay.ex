0330df585c1024aeec8eec01789f9c3d8895a439
r0cc4t0 <guilherme.roccato@outlook.com>
2021-05-28 22:57:30 -0300

    Transaction Struct

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,8 +1,9 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
-  alias Rocketpay.Accounts.{Deposit, Withdrawal}
+  alias Rocketpay.Accounts.{Deposit, Transaction, Withdrawal}
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withdrawal(params), to: Withdrawal, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end

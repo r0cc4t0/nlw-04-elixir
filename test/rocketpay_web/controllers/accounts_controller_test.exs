855fbd3b6427d8a82f1a69046beafd92188f5b57
r0cc4t0 <guilherme.roccato@outlook.com>
2021-06-02 23:24:21 -0300

    New Tests

--- test/rocketpay_web/controllers/accounts_controller_test.exs
+++ test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -51,4 +51,59 @@ defmodule RocketpayWeb.AccountsControllerTest do
       assert response == expected_response
     end
   end
+
+  describe "withdrawal/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "Rafael",
+        password: "123456",
+        nickname: "camarda",
+        email: "rafael@email.com",
+        age: 27
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = Rocketpay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic dGVzdGU6dGVzdGUxMjM=")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "When all parameters are valid, make the withdrawal.",
+         %{
+           conn: conn,
+           account_id: account_id
+         } do
+      deposit_params = %{"value" => "100.00"}
+      withdrawal_params = %{"value" => "75.00"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, deposit_params))
+        |> post(Routes.accounts_path(conn, :withdrawal, account_id, withdrawal_params))
+        |> json_response(:ok)
+
+      assert %{
+               "account" => %{"balance" => "25.00", "id" => _id},
+               "message" => "Withdrawal: Ballance changed successfully."
+             } = response
+    end
+
+    test "When there are invalid parameters, returns an error.",
+         %{
+           conn: conn,
+           account_id: account_id
+         } do
+      params = %{"value" => "test"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :withdrawal, account_id, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid value!"}
+
+      assert response == expected_response
+    end
+  end
 end
